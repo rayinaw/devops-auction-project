7cdb46db649a7064361088a551bbef3547eb7653
rayinaw <rayin.awarf@gmail.com>
2023-12-28 02:13:01 +0700

    add k8s

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -18,17 +18,6 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        // if (count == 0)
-        // {
-        //     Console.WriteLine("No data - will  attempt to seed");
-        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-
-        //     var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-            
-        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-
-        //     await DB.SaveAsync(items);
-        // }
         using var scope = app.Services.CreateScope();
 
         var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
